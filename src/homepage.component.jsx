38af3342b76969c63537969dbd0dcf526cf81880
ilukin89 <67657429+ilukin89@users.noreply.github.com>
2022-02-10 17:56:27 +0100

    Update homepage.component.jsx

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -7,7 +7,7 @@ const Homepage = () => (
 
       <div className='menu-item'>
         <div className='content'>
-          <h1 className='title'> HEADWARE</h1>
+          <h1 className='title'> HATS</h1>
           <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
